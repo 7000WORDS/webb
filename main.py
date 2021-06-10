870107f8855e56104ca397efbab1c22a98de63c1
7000words <emekadrake707@gmail.com>
2021-06-10 23:41:58 +0100

    just a lil classic more

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+@app.route('/hello/<int:score>')
+def hello_name(score):
+    return render_template('hello.html', marks = score)
 
 
 if __name__ == '__main__':
